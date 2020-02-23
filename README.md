62a771af6240d11d53a7c4ce8ab889df4908002a
upcwangying <upcwangying@126.com>
2020-02-23 21:18:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cwamp for Web
+
+![Travis (.org)](https://img.shields.io/travis/cwamp/cwamp-web-vue)
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Button type="primary">hshsh</Button>
+  </div>
+</template>
+
+<script>
+import { Button } from "ant-design-vue";
+export default {
+  name: "App",
+  components: {
+    Button
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
